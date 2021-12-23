58b0aa7e6f665e1d7371579fd42852d6f8b99673
Peterson <petersonsimiao1@gmail.com>
2021-12-23 16:15:02 -0300

    feat(provider module): Criado pipe para validar telefone

---
+++ src/providres/pipes/celphone-validation.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { CelphoneValidationPipe } from './celphone-validation.pipe';
+
+describe('CelphoneValidationPipe', () => {
+  it('should be defined', () => {
+    expect(new CelphoneValidationPipe()).toBeDefined();
+  });
+});
---
+++ src/providres/pipes/celphone-validation.pipe.ts
@@ -0,0 +1,24 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { Provider } from 'src/providers/interfaces/provider';
+
+@Injectable()
+export class CelphoneValidationPipe implements PipeTransform {
+  transform(value: Provider, metadata: ArgumentMetadata) {
+    const { celphone } = value;
+    const isSizeValid = (phone) => /\d{10,11}/.test(phone);
+    const isOnlyDigits = (phone) => !isNaN(phone);
+
+    if (!isSizeValid(celphone) || !isOnlyDigits(celphone)) {
+      throw new BadRequestException(
+        `Celphone: ${celphone} is invalid format the correct is only digits`,
+      );
+    }
+
+    return value;
+  }
+}
